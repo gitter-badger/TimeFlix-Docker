4dde14c9a6342fcedc2764d10317f3c89e21074d
Peanuts <jack.gianesini@gmail.com>
2015-06-03 14:20:00 +0200

    Update config_exemple.php

--- config/config_exemple.php
+++ config/config_exemple.php
@@ -2,7 +2,7 @@
 // Connexion SQL
 try
 {
-    $bdd = new PDO('mysql:host=localhost;dbname=timeflix', 'root', 'timeflix2015');
+    $bdd = new PDO('mysql:host=localhost;dbname=timeflix', 'root', '');
 }
 catch(Exception $e)
 {
@@ -12,10 +12,5 @@ catch(Exception $e)
 $revision =  '1.0 Beta';
 $url ="index.php";
 $title="TimeFlix";
-$imageslink = "images/";
-$videolink = "//timeflix.net/downloads/";
-$serie = '/var/www/timeflix.net/web/downloads/';
-$sabnapi = '42ef35dda9ba40faf0137ff80e9b74e3';
-$saburl = '62.210.136.116:8080';
-$key_crypt = '*mr6dRQ9T/@5Gn9c!5S-';
+$key_crypt = '';
 ?>
